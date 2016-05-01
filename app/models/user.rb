f783f620193c94838bc75b2504f3cf45f500def0
anibha88 <anibha88@gmail.com>
2016-05-01 12:51:42 +0530

    Add paperclip to app and change view files

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :plays
+
+  has_attached_file :play_img, styles: { play_index: "250*350>", play_show: "325*475>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\Z/
 end
