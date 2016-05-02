68d0beff9259a70e0f8ddf8315e8ea0bb4138b55
anibha88 <anibha88@gmail.com>
2016-05-02 19:21:54 +0530

    Review model, migrations and model associations

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,6 +1,7 @@
 class Play < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+  has_many :reviews
 
   has_attached_file :play_img, styles: { play_index: "250*350>", play_show: "325*475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\Z/
